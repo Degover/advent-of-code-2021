ae89f0ce3ce11e05d36cb7d8990272f84b3cf305
Degover <chaors2000@gmail.com>
2021-12-10 10:04:37 -0300

    Resolved day 10

--- challenges/day_10.py
+++ challenges/day_10.py
@@ -5,24 +5,38 @@ class ChunkChecker:
         '}': 1197,
         '>': 25137
     }
+    COMPLETION_POINTS = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
 
     def __init__(self):
-        self.corrupted_count = {
-            ')': 0,
-            ']': 0,
-            '}': 0,
-            '>': 0
-        }
+        self.corrupted_count = { ')': 0, ']': 0, '}': 0, '>': 0 }
+        self.completion_scores = []
 
     def read_file_input(self, file_input):
-        return 0
+        for line in file_input:
+            result = self.check_line(line)
 
+            corrupted_char = result['corrupted']
+            completion_str = result['completion']
+
+            if corrupted_char != '':
+                self.corrupted_count[corrupted_char] += 1
+            else:
+                score = self.calculate_completion_str_points(completion_str)
+                self.completion_scores.append(score)
+            
     def check_line(self, line):
         previous_chars = []
         opening_chars = '([{<'
+        completion_chars = { '(': ')', '[': ']', '{': '}', '<': '>' }
 
         result = {
-            'corrupted': ''
+            'corrupted': '',
+            'completion': ''
         }
         for char in line:
             if char == '\n': break
@@ -31,14 +45,48 @@ class ChunkChecker:
                 previous_chars.append(char)
             else:
                 last_char = previous_chars.pop()
-                if not (last_char == '(' and char == ')'
-                    or last_char == '[' and char == ']'
-                    or last_char == '{' and char == '}'
-                    or last_char == '<' and char == '>'):
+                if completion_chars[last_char] != char:
                     result['corrupted'] = char
                     break
 
+        while len(previous_chars) > 0:
+            char = previous_chars.pop()
+            result['completion'] += completion_chars[char]
+
         return result
 
+    def calculate_completion_str_points(self, completion_string):
+        total_score = 0
+        for char in completion_string:
+            char_val = self.COMPLETION_POINTS[char]
+            total_score *= 5
+            total_score += char_val
+
+        return total_score
+
     def get_total_corrupted_points(self):
-        return 0
+        total = 0
+        for key, count in self.corrupted_count.items():
+            total += self.CORRUPTED_POINTS[key] * count
+
+        return total
+
+def part1_solution(file_input):
+    checker = ChunkChecker()
+    checker.read_file_input(file_input)
+    return checker.get_total_corrupted_points()
+
+def part2_solution(file_input):
+    checker = ChunkChecker()
+    checker.read_file_input(file_input)
+    checker.completion_scores.sort()
+    middle_index = int(len(checker.completion_scores)/2)
+
+    return checker.completion_scores[middle_index]
+
+if __name__ == '__main__':
+    with open('inputs/10.txt', 'r') as file_input:
+        print(part1_solution(file_input))
+
+    with open('inputs/10.txt', 'r') as file_input:
+        print(part2_solution(file_input))
--- test/day_10_test.py
+++ test/day_10_test.py
@@ -1,5 +1,6 @@
 import unittest
 from challenges.day_10 import *
+from test.file_stub import FileStub
 
 test_input = [
     '[({(<(())[]>[[{[]{<()<>>',
@@ -49,3 +50,77 @@ class Day_10_Tests(unittest.TestCase):
         for input in test_cases:
             output = checker.check_line(input)
             self.assertEqual(output['corrupted'], '')
+
+    def testChunkChecker_CheckLine_WithIncompleteLines_ShouldReturnCorrectCompletionStrings(self):
+        test_cases = [
+            ('[({(<(())[]>[[{[]{<()<>>', '}}]])})]'),
+            ('[(()[<>])]({[<{<<[]>>(', ')}>]})'),
+            ('(((({<>}<{<{<>}{[]{[]{}', '}}>}>))))'),
+            ('{<[[]]>}<{[{[{[]{()[[[]', ']]}}]}]}>'),
+            ('<{([{{}}[<[[[<>{}]]]>[]]', '])}>')
+        ]
+
+        checker = ChunkChecker()
+        for input, expected_output in test_cases:
+            output = checker.check_line(input)
+            self.assertEqual(output['completion'], expected_output)
+
+    def testChunkChecker_ReadFileInput_WithExampleinput_ShouldCountCorruptedCorrectly(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        checker = ChunkChecker()
+        checker.read_file_input(file_stub)
+
+        expected_output = { ')': 2, ']': 1, '}': 1, '>': 1 }
+        self.assertDictEqual(checker.corrupted_count, expected_output)
+
+    def testChunkChecker_ReadFileInput_WithExampleinput_ShouldCalculateCompletionScoresCorrectly(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        checker = ChunkChecker()
+        checker.read_file_input(file_stub)
+
+        expected_output = [288957, 5566, 1480781, 995444, 294]
+        self.assertListEqual(checker.completion_scores, expected_output)
+
+    def testChunkChecker_GetTotalCorruptedPoints_WithExampleinput_ShouldReturnCorrectTotal(self):
+        checker = ChunkChecker()
+        checker.corrupted_count = { ')': 2, ']': 1, '}': 1, '>': 1 }
+
+        output = checker.get_total_corrupted_points()
+
+        expected_output = 26397
+        self.assertEqual(output, expected_output)
+
+    def testChunkChecker_CalculateCompletionStrPoints_WithExamplInput_ShouldBeCorrect(self):
+        test_cases = [
+            ('}}]])})]', 288957),
+            (')}>]})', 5566),
+            ('}}>}>))))', 1480781),
+            (']]}}]}]}>', 995444),
+            ('])}>', 294)
+        ]
+        checker = ChunkChecker()
+        for input, expected_output in test_cases:
+            output = checker.calculate_completion_str_points(input)
+            self.assertEqual(output, expected_output)
+
+    def testPart1_RunSolution_WithExampleInput_ShouldBeCorrect(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        output = part1_solution(file_stub)
+
+        expected_output = 26397
+        self.assertEqual(output, expected_output)
+
+    def testPart2_RunSolution_WithExampleInput_ShouldBeCorrect(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        output = part2_solution(file_stub)
+
+        expected_output = 288957
+        self.assertEqual(output, expected_output)

4858471d3f22039e3e6c4fa49b2dbf82cd1bd606
Degover <chaors2000@gmail.com>
2021-12-16 14:07:42 -0300

    Resolved day 14, part 2

--- challenges/day_14.py
+++ challenges/day_14.py
@@ -1,8 +1,13 @@
+Counter = dict[str:int]
+MemorizedCount = dict[tuple[str,str,int]:Counter]
+
 class Polymerator:
 
     def __init__(self):
         self.rules = {}
         self.template = []
+        self.letter_counts: Counter = {}
+        self.memorized_iters: MemorizedCount = {}
 
     def read_file_input(self, file_input) -> None:
         self.template = list([ char for char in file_input.readline() if char != '\n' ])
@@ -11,56 +16,73 @@ class Polymerator:
         for line in file_input:
             self.rules[(line[0], line[1])] = line[6]
 
-    def do_steps(self, total_stelps: int) -> list[str]:
-        last_arr = self.template
-        arr = []
-        for _ in range(1, total_stelps+1):
+    def count_by_steps(self, step_count: int) -> Counter:
+        for letter in self.template:
+            self.count_letter(letter)
 
-            last_row_len = len(last_arr)
-            for i in range(last_row_len):
-                if i == last_row_len-1:
-                    arr.append(self.template[-1])
-                else:
-                    arr.append(last_arr[i])
-                    arr.append(self.rules[(last_arr[i], last_arr[i+1])])
+        for i in range(len(self.template)-1):
+            self.step_into(self.template[i], self.template[i+1], step_count)
 
-            last_arr = arr
-            arr = []
+        return self.letter_counts
+
+    def step_into(self, letter_1: str, letter_2: str, steps_left: int) -> None:
+        mid_letter = self.rules[(letter_1, letter_2)]
+        self.count_letter(mid_letter)
         
-        return last_arr
+        memorized_key = (letter_1, letter_2, steps_left)
+        if memorized_key in self.memorized_iters:
+            counter = self.memorized_iters[memorized_key]
+            self.sum_counter(counter)
+            return
+
+        if steps_left == 1:
+            return
+        else:
+            current_count = self.letter_counts.copy()
+
+            self.step_into(letter_1, mid_letter, steps_left-1)
+            self.step_into(mid_letter, letter_2, steps_left-1)
+
+            self.memorize_iter(letter_1, letter_2, steps_left, current_count, self.letter_counts)
+
+    def memorize_iter(self, letter_1: str, letter_2: str, steps_left: int, old_counts: Counter, new_counts: Counter) -> None:
+        diff_count: Counter = {}
+        for letter, count in new_counts.items():
+            if letter in old_counts:
+                if count > old_counts[letter]:
+                    diff_count[letter] = count - old_counts[letter]
+            else:
+                diff_count[letter] = count
+            
+        self.memorized_iters[(letter_1, letter_2, steps_left)] = diff_count
+
+    def sum_counter(self, counter: Counter) -> None:
+        for letter, count in counter.items():
+            if letter in self.letter_counts:
+                self.letter_counts[letter] += count
+            else:
+                self.letter_counts[letter] = count
+
+    def count_letter(self, letter: str) -> None:
+        if letter in self.letter_counts:
+            self.letter_counts[letter] += 1
+        else:
+            self.letter_counts[letter] = 1
 
 def part1_solution(file_input):
     polymerator = Polymerator()
     polymerator.read_file_input(file_input)
-    result = polymerator.do_steps(10)
+    result = polymerator.count_by_steps(10)
 
-    counter = {}
-    def increment_count(letter: str):
-        nonlocal counter
-        if letter in counter:
-            counter[letter] += 1
-        else:
-            counter[letter] = 1
-
-    list([ increment_count(letter) for letter in result])
-    counts = counter.values()
+    counts = result.values()
     return max(counts) - min(counts)
     
 def part2_solution(file_input):
     polymerator = Polymerator()
     polymerator.read_file_input(file_input)
-    result = polymerator.do_steps(40)
+    result = polymerator.count_by_steps(40)
 
-    counter = {}
-    def increment_count(letter: str):
-        nonlocal counter
-        if letter in counter:
-            counter[letter] += 1
-        else:
-            counter[letter] = 1
-
-    list([ increment_count(letter) for letter in result])
-    counts = counter.values()
+    counts = result.values()
     return max(counts) - min(counts)
 
 if __name__ == '__main__':
--- test/day_14_test.py
+++ test/day_14_test.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.case import expectedFailure
 from challenges.day_14 import *
 from test.file_stub import FileStub
 
@@ -47,17 +48,107 @@ class Day_14_Tests(unittest.TestCase):
         }
         self.assertDictEqual(polymerator.rules, expected_rules)
 
-    def testPolymerator_DoSteps_WithSingleStep_ShouldReturnCorrectly(self):
+    def testPolymerator_CountLetter_WithUnexistingLetter_ShouldInsertIt(self):
+        polymerator = Polymerator()
+
+        polymerator.count_letter('A')
+
+        expected_count = { 'A': 1 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
+
+    def testPolymerator_CountLetter_WithExistingLetter_ShouldIncrementIt(self):
+        polymerator = Polymerator()
+        polymerator.letter_counts = { 'A': 3 }
+
+        polymerator.count_letter('A')
+
+        expected_count = { 'A': 4 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
+
+    def testPolymerator_StepInto_WithSingleStep_ShouldCountCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.rules = { ('N', 'N'): 'B' }
+
+        polymerator.step_into('N', 'N', 1)
+
+        expected_count = { 'B': 1 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
+
+    def testPolymerator_StepInto_WithThreeSteps_ShouldCountCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.rules = {
+            ('N', 'N'): 'B',
+            ('N', 'B'): 'B',
+            ('B', 'N'): 'B',
+            ('B', 'B'): 'N'
+        }
+
+        polymerator.step_into('N', 'N', 3)
+
+        expected_count = { 'N': 2, 'B': 5 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
+
+    def testPolymerator_StepInto_WithExampleInput_ShouldCountCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.rules = {
+            ('C', 'H'): 'B',
+            ('H', 'H'): 'N',
+            ('C', 'B'): 'H',
+            ('N', 'H'): 'C',
+            ('H', 'B'): 'C',
+            ('H', 'C'): 'B',
+            ('H', 'N'): 'C',
+            ('N', 'N'): 'C',
+            ('B', 'H'): 'H',
+            ('N', 'C'): 'B',
+            ('N', 'B'): 'B',
+            ('B', 'N'): 'B',
+            ('B', 'B'): 'N',
+            ('B', 'C'): 'B',
+            ('C', 'C'): 'N',
+            ('C', 'N'): 'C',
+        }
+        
+        polymerator.step_into('N', 'N', 4)
+
+        expected_count = { 'N': 4, 'B': 6, 'C': 5 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
+
+    def testPolymerator_StepInto_WithMemorizedIter_ShouldCountCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.memorized_iters = { ('A', 'A', 3): { 'B': 2, 'A': 1 } }
+        polymerator.rules = { ('A', 'A'): 'B' }
+
+        polymerator.step_into('A', 'A', 3)
+
+        expected_counter = { 'B': 3, 'A': 1 }
+        self.assertDictEqual(polymerator.letter_counts, expected_counter)
+
+    def testPolymerator_StepInto_WithIterToMemorize_ShouldCountCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.rules = {
+            ('A', 'A'): 'B',
+            ('B', 'A'): 'B',
+            ('A', 'B'): 'B',
+            ('B', 'B'): 'B'
+        }
+
+        polymerator.step_into('A', 'A', 4)
+
+        expected_counter = { 'B': 15 }
+        self.assertDictEqual(polymerator.letter_counts, expected_counter)
+
+    def testPolymerator_CountBySteps_WithSingleStep_ShouldCountCorrectly(self):
         polymerator = Polymerator()
         polymerator.rules = { ('N', 'N'): 'B' }
         polymerator.template = ['N', 'N']
 
-        output = polymerator.do_steps(1)
+        output = polymerator.count_by_steps(1)
 
-        expected_output = ['N', 'B', 'N']
-        self.assertListEqual(output, expected_output)
+        expected_output = { 'N': 2, 'B': 1 }
+        self.assertDictEqual(output, expected_output)
 
-    def testPolymerator_DoSteps_WithThreeSteps_ShouldReturnCorrectly(self):
+    def testPolymerator_CountBySteps_WithThreeSteps_ShouldCountCorrectly(self):
         polymerator = Polymerator()
         polymerator.rules = {
             ('N', 'N'): 'B',
@@ -67,12 +158,12 @@ class Day_14_Tests(unittest.TestCase):
         }
         polymerator.template = ['N', 'N']
 
-        output = polymerator.do_steps(3)
+        output = polymerator.count_by_steps(3)
 
-        expected_output = ['N', 'B', 'B', 'N', 'B', 'N', 'B', 'B', 'N']
-        self.assertListEqual(output, expected_output)
+        expected_output = { 'N': 4, 'B': 5 }
+        self.assertDictEqual(output, expected_output)
 
-    def testPolymerator_DoSteps_WithExampleInput_ShouldReturnCorrectly(self):
+    def testPolymerator_CountBySteps_WithExampleInput_ShouldCountCorrectly(self):
         polymerator = Polymerator()
         polymerator.rules = {
             ('C', 'H'): 'B',
@@ -94,12 +185,45 @@ class Day_14_Tests(unittest.TestCase):
         }
         polymerator.template = ['N', 'N', 'C', 'B']
 
-        output = polymerator.do_steps(4)
+        output = polymerator.count_by_steps(4)
 
-        expected_output = ['N', 'B', 'B', 'N', 'B', 'N', 'B', 'B', 'C', 'C', 'N', 'B', 'C', 'N', 'C', 'C', 'N', 'B',
-            'B', 'N', 'B', 'B', 'N', 'B', 'B', 'B', 'N', 'B', 'B', 'N', 'B', 'B', 'C', 'B', 'H', 'C', 'B', 'H', 'H',
-            'N', 'H', 'C', 'B', 'B', 'C', 'B', 'H', 'C', 'B']
-        self.assertListEqual(output, expected_output)
+        expected_output = {'N': 11, 'B': 23, 'C': 10, 'H': 5}
+        self.assertDictEqual(output, expected_output)
+
+    def testPolymerator_MemorizeIter_WithEmptyOldCount_ShouldMemorizeCorrectly(self):
+        new_counts = { 'A': 2, 'B': 3 }
+
+        polymerator = Polymerator()
+        polymerator.memorize_iter('A', 'A', 5, {}, new_counts)
+
+        expected_memorized = { ('A', 'A', 5): new_counts }
+        self.assertDictEqual(polymerator.memorized_iters, expected_memorized)
+
+    def testPolymerator_MemorizeIter_WithFilledOldCount_ShouldMemorizeCorrectly(self):
+        new_counts = { 'A': 2, 'B': 3 }
+
+        polymerator = Polymerator()
+        polymerator.memorize_iter('A', 'A', 5, { 'A': 1 }, new_counts)
+
+        expected_memorized = { ('A', 'A', 5): { 'A': 1, 'B': 3 } }
+        self.assertDictEqual(polymerator.memorized_iters, expected_memorized)
+
+    def testPolymerator_SumCounter_WithEmptyOldCount_ShouldSumCorrectly(self):
+        new_counts = { 'A': 2, 'B': 3 }
+
+        polymerator = Polymerator()
+        polymerator.sum_counter(new_counts)
+
+        self.assertDictEqual(polymerator.letter_counts, new_counts)
+
+    def testPolymerator_SumCounter_WithFilledOldCount_ShouldSumCorrectly(self):
+        polymerator = Polymerator()
+        polymerator.letter_counts = { 'A': 1 }
+
+        polymerator.sum_counter({ 'A': 2, 'B': 3 })
+
+        expected_count = { 'A': 3, 'B': 3 }
+        self.assertDictEqual(polymerator.letter_counts, expected_count)
 
     def testPart1_RunSolution_WithExampleInput_ShouldBeCorrect(self):
         file_stub = FileStub()

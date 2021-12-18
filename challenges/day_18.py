7a9b4a0cb73ce47a4103f5fc2feecfe8a48c7ae1
Degover <chaors2000@gmail.com>
2021-12-18 07:15:31 -0300

    Created files to resolve the day 18 puzzle

---
+++ challenges/day_18.py
@@ -0,0 +1,12 @@
+def part1_solution(file_input):
+    return 0
+
+def part2_solution(file_input):
+    return 0
+
+if __name__ == '__main__':
+    with open('inputs/18.txt', 'r') as file_input:
+        print(part1_solution(file_input))
+
+    with open('inputs/18.txt', 'r') as file_input:
+        print(part2_solution(file_input))
---
+++ test/day_18_test.py
@@ -0,0 +1,37 @@
+import unittest
+from challenges.day_18 import *
+from test.file_stub import FileStub
+
+test_input = [ 
+    '[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]',
+    '[[[5,[2,8]],4],[5,[[9,9],0]]]',
+    '[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]',
+    '[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]',
+    '[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]',
+    '[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]',
+    '[[[[5,4],[7,7]],8],[[8,3],8]]',
+    '[[9,3],[[9,9],[6,[4,9]]]]',
+    '[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]',
+    '[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]',
+]
+
+class Day_18_Tests(unittest.TestCase):
+    
+    def testPart1_RunSolution_WithExampleInput_ShouldBeCorrect(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        output = part1_solution(file_stub)
+
+        expected_output = 4140
+        self.assertEqual(output, expected_output)
+
+    def testPart2_RunSolution_WithExampleInput_ShouldBeCorrect(self):
+        file_stub = FileStub()
+        file_stub.set_array(test_input)
+
+        output = part2_solution(file_stub)
+
+        expected_output = -1
+        self.assertEqual(output, expected_output)
+        
